abaf290d3ce285e5fab5ac90a9bda87e7904f24c
tsela85 <tsela85@gmail.com>
2010-12-11 20:13:41 +0200

    added all files

---
+++ include/ReadFile.h
@@ -0,0 +1,46 @@
+// READFILE.h
+
+#ifndef READFILE_H
+#define READFILE_H
+
+#include <string>
+#include <vector>
+
+#include "Poco/Util/IniFileConfiguration.h"
+#include "Poco/DateTime.h"
+#include "Poco/DateTimeParser.h"
+
+#include "../include/HRC.h"
+
+class ReadFile : public Poco::Util::IniFileConfiguration
+{
+public:
+	Poco::DateTime *time;
+	// Constructor that receive data stream as std::istream
+	ReadFile(std::istream& istr);
+
+	// Constructor that receive path as std::string
+	ReadFile(const std::string& path);
+	/////////////////////////////////////////////////////////////////////////
+
+	// Destructor
+	virtual ~ReadFile();
+
+	void getConfig(Poco::DateTime *date, int *jobNum, int *workNum,int *seekerRep
+			,int *companyRep,int *HrcStrag,int *logConP,int *logfileP);
+
+	int getWorkers(std::vector<s_p_Worker> *workers);
+
+	int getCompanies(std::vector<s_p_Company> *companies);
+
+	int getJobs(std::vector<s_p_Job> *jobs);
+
+	void SetTime(Poco::DateTime *_time) {
+		time=_time;
+	}
+
+
+};
+
+#endif //
+
---
+++ src/ReadFile.cpp
@@ -0,0 +1,148 @@
+// ReadFile.cpp
+
+#include <sstream>
+
+#include "Poco/DateTime.h"
+#include "Poco/DateTimeParser.h"
+#include <boost/algorithm/string.hpp>
+#include <boost/lexical_cast.hpp>
+
+#include "../include/ReadFile.h"
+#include "../h/AppLogger.h"
+
+using namespace Poco;
+using namespace Poco::Util;
+using namespace std;
+
+int switchToInt(string temp);
+
+// Constructor that receive data stream as std::istream
+ReadFile::ReadFile(std::istream& istr) : IniFileConfiguration(istr) {
+
+}
+
+// Constructor that receive path as std::string
+ReadFile::ReadFile(const std::string& path) : IniFileConfiguration(path){
+}
+
+ReadFile::~ReadFile(){
+}
+
+void ReadFile::getConfig(Poco::DateTime *_date, int *_jobNum, int *_workNum,int *_seekerRep
+		,int *_companyRep,int *_HrcStrat,int *_logConP,int *_logFileP) {
+
+	ostringstream streamDate;
+	ostringstream streamJobNum;
+	ostringstream streamWorkNum;
+	ostringstream streamSeekerRep;
+	ostringstream streamCompanyRep;
+	ostringstream streamHrcStrat;
+	ostringstream streamLogConp;
+	ostringstream streamLogFileP;
+
+	// Get  INITIAL_DATE
+	streamDate << "GENERAL.INITIAL_DATE";
+	string dateStr =
+			getString(streamDate.str());
+	int timeZoneDiff;
+	DateTime date;
+	DateTimeParser::tryParse("%d/%n/%Y", dateStr, date, timeZoneDiff);
+	*_date=date;
+
+	// Get job number
+	streamJobNum << "GENERAL.INIT_JOBS_NUM";
+	*_jobNum = getInt(streamJobNum.str());
+
+	// Get work number
+	streamWorkNum << "GENERAL.INIT_CANDS_NUM";
+	*_workNum = getInt(streamWorkNum.str());
+
+	// Get seeker rep
+	streamSeekerRep << "GENERAL.INIT_SEEKER_REP";
+	*_seekerRep = getInt(streamSeekerRep.str());
+
+	// Get company rep
+	streamCompanyRep << "GENERAL.INIT_COMPANY_REP";
+	*_companyRep = getInt(streamCompanyRep.str());
+
+	// Get HRC strategy
+	streamHrcStrat << "GENERAL.HRC_STRATEGY";
+	string stratString=getString(streamHrcStrat.str());
+	if (stratString == "EAGER")
+		*_HrcStrat = EAGER;
+	else
+		if (stratString == "FAIR")
+			*_HrcStrat = FAIR;
+	//   else TODO: throw exception error reading
+
+	// Get LOGGER_CONSOLE_PRIORITY
+	streamLogConp << "GENERAL.LOGGER_CONSOLE_PRIORITY";
+	*_logConP = getInt(streamLogConp.str());
+
+	// Get LOGGER_FILE_PRIORITY
+	streamLogFileP << "GENERAL.LOGGER_FILE_PRIORITY";
+	*_logFileP = getInt(streamLogFileP.str());
+}
+
+int ReadFile::getWorkers(std::vector<s_p_Worker> *_workers) {
+	ostringstream sAmount;
+	sAmount << "numberOfCANDIDATES";
+	int amount = getInt(sAmount.str());
+	for (int i=1; i <= amount; i++) {
+		// ID
+		ostringstream sID;
+		sID << "CANDIDATE" << i << ".ID";
+		int ID = boost::lexical_cast<int>(getString(sID.str()));
+		// Skills
+		ostringstream sSkillNum;
+		sSkillNum << "CANDIDATE" << i << ".numberOfSkills";
+		int skillNum = getInt(sSkillNum.str());
+		int skills[6];
+		for(int k=0;k < 6;k++) skills[k]=0;
+		for(int j=1; j <= skillNum;j++) {
+			ostringstream sSkill;
+			sSkill << "CANDIDATE" << i << ".Skill" << j;
+			string skill= getString(sSkill.str());
+			vector<string> parts;
+			boost::algorithm::split(parts,skill,boost::is_any_of(" "));
+			skills[switchToInt(parts[0])]=boost::lexical_cast<int>(parts[1]);
+		}
+		// Expected_Salary
+		ostringstream sSalary;
+		sSalary << "CANDIDATE" << i << ".Expected_Salary";
+		float salary = getDouble(sSalary.str());
+		// Desired_Job_Type
+		bool jobs[6];
+		for(int k=0;k < 6;k++) jobs[k]=false;
+		ostringstream sjobs;
+		sjobs << "CANDIDATE" << i << ".Desired_Job_Type";
+		string job= getString(sjobs.str());
+		vector<string> parts;
+		boost::algorithm::split(parts,job,boost::is_any_of(" "));
+		for (vector<string>::iterator it = parts.begin(); it != parts.end(); it++)
+			jobs[switchToInt(*it)]=true;
+		// create a worker
+		s_p_Worker worker(new Worker(ID,skills,jobs,salary,time));
+		_workers->push_back(worker);
+	}
+
+	return amount;
+}
+
+int switchToInt(string temp) {
+	if (temp == "cpp") return _CPP;
+	if (temp == "java") return _JAVA;
+	if (temp == "script") return _SCRIPT;
+	if (temp == "web") return _WEB;
+	if (temp == "system") return _SYSTEM;
+	if (temp == "qa") return _QA;
+
+	if (temp == "data_storage") return _DATA_STORAGE;
+	if (temp == "digital_media") return _DIGITAL_MEDIA;
+	if (temp == "e_commerce") return _E_COMMERCE ;
+	if (temp == "gaming") return _GAMING;
+	if (temp == "rt") return _RT;
+	if (temp == "security") return _SECURITY;
+	return -1; // TODO: throw error
+}
+
